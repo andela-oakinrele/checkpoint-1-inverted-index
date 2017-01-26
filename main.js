c145351a67d7618d97abb2c8a4a587dd18714da1
andela-oakinrele <oluwasinmisola.akinrele@andela.com>
2017-01-26 09:59:57 +0100

    Express for heroku

---
+++ main.js
@@ -0,0 +1,9 @@
+const express = require('express');
+
+const app = express();
+
+app.use(express.static((__dirname)));
+
+app.listen(process.env.PORT || 5000, () => {
+  console.log('Server listening on port 5000');
+});
